ed3228af753ca3217f08f3d69b539b456cae8d98
Aashim Limbu <thelastmanarmy37@gmail.com>
2023-07-11 09:54:21 +0545

    stripe added

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,22 +1,23 @@
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 import Button, { BUTTON_TYPE } from "../Buttons/button.component";
 import { FormContainer, PaymentFormContainer } from "./payment-form.style";
 export default function PaymentForm() {
 	const stripe = useStripe();
 	const elements = useElements();
+
 	async function paymentHandler(e) {
 		e.preventDefault();
 		if (!stripe || !elements) {
 			return;
 		}
-		const response = await fetch(`/.netlify/functions/create-payment-intent`, {
+		const response = await fetch("/.netlify/functions/create-payment-intent", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
 			},
-			body: JSON.stringify({ amount: 10000 }),
-		}).then((res) =>res.json());
-		console.log(response);
+			body: JSON.stringify({ amount: 100 }),
+		}).then((res) => res.json());
+		console.log("this is the response -> ", response);
 	}
 	return (
 		<PaymentFormContainer>
